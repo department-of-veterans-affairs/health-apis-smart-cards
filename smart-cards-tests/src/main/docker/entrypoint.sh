894c19898203e62e72fe10c81d7df507fa19c641
Alberto Peirats <72031660+apeirats-va@users.noreply.github.com>
2021-02-05 13:20:09 -0500

    integ tests allow override internal url

--- smart-cards-tests/src/main/docker/entrypoint.sh
+++ smart-cards-tests/src/main/docker/entrypoint.sh
@@ -6,13 +6,22 @@ if [ -z "${SENTINEL_BASE_DIR:-}" ]; then SENTINEL_BASE_DIR=/sentinel; fi
 cd $SENTINEL_BASE_DIR
 
 test -n "${K8S_ENVIRONMENT}"
-if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV=$K8S_ENVIRONMENT; fi
+test -n "${K8S_LOAD_BALANCER}"
 
-java-tests \
-  --module-name "smart-cards-tests" \
-  --regression-test-pattern ".*IT\$" \
-  --smoke-test-pattern ".*Smoke.*IT\$" \
-  -Dsentinel="$SENTINEL_ENV" \
-  $@
+main() {}
+  if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV=$K8S_ENVIRONMENT; fi
+  if [ -z "${SC_INTERNAL_URL:-}" ]; then SC_INTERNAL_URL="https://${K8S_LOAD_BALANCER}"; fi
 
-exit $?
+  SYSTEM_PROPERTIES="-Dsentinel=${SENTINEL_ENV} -Dsentinel.internal.url=${SC_INTERNAL_URL}"
+
+  java-tests \
+    --module-name "smart-cards-tests" \
+    --regression-test-pattern ".*IT\$" \
+    --smoke-test-pattern ".*Smoke.*IT\$" \
+    $SYSTEM_PROPERTIES \
+    $@
+
+  exit $?
+}
+
+main $@

975a1d88ad17a14faa5708c18a6eea816e820c2b
apeirats-va <72031660+apeirats-va@users.noreply.github.com>
2021-02-05 10:32:32 -0500

    API-4722 initial skeleton (#1)

---
+++ smart-cards-tests/src/test/java/gov/va/api/health/smartcards/tests/SmokeIT.java
@@ -0,0 +1,27 @@
+package gov.va.api.health.smartcards.tests;
+
+import static gov.va.api.health.smartcards.tests.SystemDefinitions.systemDefinition;
+
+import gov.va.api.health.sentinel.ExpectedResponse;
+import gov.va.api.health.smartcards.tests.SystemDefinitions.Service;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class SmokeIT {
+  @Test
+  void health() {
+    Service svc = systemDefinition().internal();
+    String request = svc.urlWithApiPath() + "actuator/health";
+    log.info(request);
+    ExpectedResponse.of(
+            RestAssured.given()
+                .baseUri(svc.url())
+                .port(svc.port())
+                .relaxedHTTPSValidation()
+                .request(Method.GET, request))
+        .expect(200);
+  }
+}

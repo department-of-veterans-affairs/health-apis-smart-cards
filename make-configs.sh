291095d6a975c4373c66be021ee7bb391118692a
Alberto Peirats <72031660+apeirats-va@users.noreply.github.com>
2021-02-11 17:09:06 -0500

    make configs

---
+++ make-configs.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+
+REPO=$(cd $(dirname $0) && pwd)
+PROFILE=dev
+MARKER=$(date +%s)
+
+makeConfig() {
+  local project="$1"
+  local profile="$2"
+  local target="$REPO/$project/config/application-${profile}.properties"
+  [ -f "$target" ] && mv -v $target $target.$MARKER
+  grep -E '(.*= *unset)' "$REPO/$project/src/main/resources/application.properties" \
+    > "$target"
+}
+
+configValue() {
+  local project="$1"
+  local profile="$2"
+  local key="$3"
+  local value="$4"
+  local target="$REPO/$project/config/application-${profile}.properties"
+  local escapedValue=$(echo $value | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')
+  sed -i "s/^$key=.*/$key=$escapedValue/" $target
+}
+
+checkForUnsetValues() {
+  local project="$1"
+  local profile="$2"
+  local target="$REPO/$project/config/application-${profile}.properties"
+  echo "checking $target"
+  grep -E '(.*= *unset)' "$target"
+  [ $? == 0 ] && echo "Failed to populate all unset values" && exit 1
+  diff -q $target $target.$MARKER
+  [ $? == 0 ] && rm -v $target.$MARKER
+}
+
+makeConfig smart-cards $PROFILE
+configValue smart-cards $PROFILE public-r4-base-path 'r4'
+configValue smart-cards $PROFILE public-url 'http://localhost:8096'
+configValue smart-cards $PROFILE web-exception-key '-sharktopus-v-pteracuda-'
+checkForUnsetValues smart-cards $PROFILE

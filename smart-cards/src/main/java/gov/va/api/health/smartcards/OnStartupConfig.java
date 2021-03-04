294302146be3d39a8b3eb287b97f4c7b9f3ebbca
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2021-03-04 09:44:57 -0500

    more hardcoding

--- smart-cards/src/main/java/gov/va/api/health/smartcards/OnStartupConfig.java
+++ smart-cards/src/main/java/gov/va/api/health/smartcards/OnStartupConfig.java
@@ -5,26 +5,15 @@ import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-@Component
 @Slf4j
+@Component
 public class OnStartupConfig {
   private static final String PATIENT_IDENTIFIER_SIZE_PROPERTY =
       Patient.class.getName() + ".identifier.min-size";
 
   @PostConstruct
   void init() {
-    overridePatientIdentifierMinSize();
-  }
-
-  private void overridePatientIdentifierMinSize() {
-    // JVM Property takes precedence
-    if (System.getProperty(PATIENT_IDENTIFIER_SIZE_PROPERTY) == null) {
-      Patient.IDENTIFIER_MIN_SIZE.set(0);
-    }
-
-    log.info(
-        "R4 Patient resource identifier.min-size set to {}. Override with -D{}",
-        Patient.IDENTIFIER_MIN_SIZE.get(),
-        PATIENT_IDENTIFIER_SIZE_PROPERTY);
+    Patient.IDENTIFIER_MIN_SIZE.set(0);
+    log.info("{} set to 0", PATIENT_IDENTIFIER_SIZE_PROPERTY);
   }
 }

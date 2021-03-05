db36673fd9bf48d5313cdce67dcec3c2b89da6fc
Adrian Garcia <adrian.garcia@libertyits.com>
2021-03-05 13:31:13 -0500

    Removed superfluous comments, and added additional test for a bad request.

--- smart-cards/src/test/java/gov/va/api/health/smartcards/DataQueryFhirClientTest.java
+++ smart-cards/src/test/java/gov/va/api/health/smartcards/DataQueryFhirClientTest.java
@@ -1,14 +1,14 @@
 package gov.va.api.health.smartcards;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.google.common.io.Resources;
 import gov.va.api.health.r4.api.resources.Patient;
-import java.nio.charset.StandardCharsets;
+import gov.va.api.health.smartcards.Exceptions.FhirClientConnectionFailure;
 import java.util.HashMap;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
@@ -20,19 +20,23 @@ import org.springframework.web.client.RestTemplate;
 
 public class DataQueryFhirClientTest {
 
-  @SneakyThrows
-  public static String contentOf(String resource) {
-    return Resources.toString(
-        DataQueryFhirClientTest.class.getResource(resource), StandardCharsets.UTF_8);
+  @Test
+  void makesBadRequest() {
+    RestTemplate restTemplate = mock(RestTemplate.class);
+    LinkProperties linkProperties = mock(LinkProperties.class);
+    DataQueryFhirClient dataQueryFhirClient = new DataQueryFhirClient(restTemplate, linkProperties);
+    var response = new ResponseEntity<>(Patient.Bundle.builder().build(), HttpStatus.BAD_REQUEST);
+    when(restTemplate.exchange(
+            any(String.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(Patient.Bundle.class)))
+        .thenReturn(response);
+    assertThatThrownBy(
+            () -> dataQueryFhirClient.patientBundle("123", new HashMap<String, String>()))
+        .isInstanceOf(FhirClientConnectionFailure.class);
   }
-
-  // mapper.writeValueAsString(Patient.Bundle.builder().build()),
-  // mapper.readValue(contentOf("/patient-bundle.json"), Patient.Bundle.class)
 
   @Test
   @SneakyThrows
   void makesRequests() {
-    //    var mapper = JacksonMapperConfig.createMapper();
     RestTemplate restTemplate = mock(RestTemplate.class);
     LinkProperties linkProperties = mock(LinkProperties.class);
     DataQueryFhirClient dataQueryFhirClient = new DataQueryFhirClient(restTemplate, linkProperties);

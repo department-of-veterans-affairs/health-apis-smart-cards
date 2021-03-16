224d78676799c644adc3864a8813a54191c5dbb2
Alberto Peirats <72031660+apeirats-va@users.noreply.github.com>
2021-03-16 16:46:37 -0400

    unnecessary annotations

--- smart-cards/src/main/java/gov/va/api/health/smartcards/vc/PayloadClaimsWrapper.java
+++ smart-cards/src/main/java/gov/va/api/health/smartcards/vc/PayloadClaimsWrapper.java
@@ -1,8 +1,6 @@
 package gov.va.api.health.smartcards.vc;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,11 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor
-@JsonAutoDetect(
-    fieldVisibility = JsonAutoDetect.Visibility.ANY,
-    isGetterVisibility = JsonAutoDetect.Visibility.NONE)
-@Schema
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PayloadClaimsWrapper {
   @JsonProperty("iss")
   String issuer;

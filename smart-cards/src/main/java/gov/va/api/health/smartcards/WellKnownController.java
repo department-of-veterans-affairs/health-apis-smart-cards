1c641449ce562b30e7ddcc53375dc59185f682d0
Adrian Garcia <adrian.garcia@libertyits.com>
2021-03-19 10:53:13 -0400

    Removed WellKnownProperties and hardcoded values into WellKnownController instead. Adapted test accordingly.

--- smart-cards/src/main/java/gov/va/api/health/smartcards/WellKnownController.java
+++ smart-cards/src/main/java/gov/va/api/health/smartcards/WellKnownController.java
@@ -1,7 +1,9 @@
 package gov.va.api.health.smartcards;
 
 import gov.va.api.health.r4.api.information.WellKnown;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +12,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(
     value = {"/.well-known/smart-configuration"},
-    produces = {"application/json", "application/fhir+json", "application/json+fhir"})
+    produces = {"application/json", "application/fhir+json"})
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
+@Builder
 public class WellKnownController {
 
-  private final WellKnownProperties wellKnownProperties;
+  @Builder.Default
+  private List<String> capabilities =
+      List.of(
+          "health-cards",
+          "launch-standalone",
+          "context-standalone-patient",
+          "client-confidential-symmetric");
+
+  @Builder.Default private List<String> responseTypeSupported = List.of("code", "refresh_token");
+
+  @Builder.Default
+  private List<String> scopesSupported =
+      List.of(
+          "launch",
+          "launch/patient",
+          "patient/Patient.read",
+          "patient/Immunization.read",
+          "patient/Location.read",
+          "offline_access");
 
   @GetMapping
   WellKnown read() {
     return WellKnown.builder()
-        .capabilities(wellKnownProperties.getCapabilities())
-        .responseTypeSupported(wellKnownProperties.getResponseTypeSupported())
-        .scopesSupported(wellKnownProperties.getScopesSupported())
+        .capabilities(capabilities)
+        .responseTypeSupported(responseTypeSupported)
+        .scopesSupported(scopesSupported)
         .build();
   }
 }
--- smart-cards/src/test/java/gov/va/api/health/smartcards/WellKnownControllerTest.java
+++ smart-cards/src/test/java/gov/va/api/health/smartcards/WellKnownControllerTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import gov.va.api.health.autoconfig.configuration.JacksonConfig;
 import gov.va.api.health.r4.api.information.WellKnown;
+import java.util.List;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
@@ -32,20 +33,20 @@ public class WellKnownControllerTest {
   @Test
   @SneakyThrows
   public void read() {
-    WellKnownController controller = new WellKnownController(wellKnownProperties());
+    List<String> capabilities =
+        List.of(
+            "context-standalone-patient",
+            "launch-standalone",
+            "permission-offline",
+            "permission-patient");
+    List<String> responseTypeSupported = List.of("code", "refresh-token");
+    List<String> scopesSupported = List.of("patient/$HealthWallet.issueVc");
+    WellKnownController controller =
+        WellKnownController.builder()
+            .capabilities(capabilities)
+            .responseTypeSupported(responseTypeSupported)
+            .scopesSupported(scopesSupported)
+            .build();
     assertThat(pretty(controller.read())).isEqualTo(pretty(actual()));
-  }
-
-  private WellKnownProperties wellKnownProperties() {
-    return WellKnownProperties.builder()
-        .capabilities(
-            asList(
-                "context-standalone-patient",
-                "launch-standalone",
-                "permission-offline",
-                "permission-patient"))
-        .responseTypeSupported(asList("code", "refresh-token"))
-        .scopesSupported(asList("patient/$HealthWallet.issueVc"))
-        .build();
   }
 }

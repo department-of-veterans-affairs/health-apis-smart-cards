cf17b3dbf366a33f82dee8b146db86c184d8ed01
Adrian Garcia <adrian.garcia@libertyits.com>
2021-03-23 18:09:14 -0400

    Added DSTU2 endpoint for metadata, and reorganized metadata class accordingly. Also added relevant tests.

--- smart-cards/src/main/java/gov/va/api/health/smartcards/MetadataController.java
+++ smart-cards/src/main/java/gov/va/api/health/smartcards/MetadataController.java
@@ -2,6 +2,14 @@ package gov.va.api.health.smartcards;
 
 import static java.util.stream.Collectors.toList;
 
+import gov.va.api.health.dstu2.api.elements.Reference;
+import gov.va.api.health.dstu2.api.resources.Conformance;
+import gov.va.api.health.dstu2.api.resources.Conformance.Kind;
+import gov.va.api.health.dstu2.api.resources.Conformance.ResourceInteractionCode;
+import gov.va.api.health.dstu2.api.resources.Conformance.RestMode;
+import gov.va.api.health.dstu2.api.resources.Conformance.RestResource;
+import gov.va.api.health.dstu2.api.resources.Conformance.RestResourceVersion;
+import gov.va.api.health.dstu2.api.resources.Conformance.Status;
 import gov.va.api.health.r4.api.datatypes.ContactDetail;
 import gov.va.api.health.r4.api.datatypes.ContactPoint;
 import gov.va.api.health.r4.api.resources.CapabilityStatement;
@@ -17,9 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(
-    value = {"/r4/metadata", "/dstu2/metadata"},
-    produces = {"application/json", "application/fhir+json"})
+@RequestMapping
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class MetadataController {
   private static final String NAME = "API Management Platform | Smart Cards - R4";
@@ -41,6 +47,21 @@ public class MetadataController {
             .build());
   }
 
+  private static List<Conformance.Contact> contactDstu2() {
+    return List.of(
+        Conformance.Contact.builder()
+            .name("API Support")
+            .telecom(
+                List.of(
+                    gov.va.api.health.dstu2.api.datatypes.ContactPoint.builder()
+                        .system(
+                            gov.va.api.health.dstu2.api.datatypes.ContactPoint.ContactPointSystem
+                                .email)
+                        .value("api@va.gov")
+                        .build()))
+            .build());
+  }
+
   private static List<CapabilityStatement.CapabilityResource> resources() {
     return Stream.of(
             SupportedResource.builder()
@@ -48,6 +69,16 @@ public class MetadataController {
                 .profileUrl("https://www.hl7.org/fhir/r4/parameters.html")
                 .build())
         .map(SupportedResource::asResource)
+        .collect(toList());
+  }
+
+  private static List<Conformance.RestResource> resourcesDstu2() {
+    return Stream.of(
+            SupportedResource.builder()
+                .type("Parameters")
+                .profileUrl("https://www.hl7.org/fhir/r4/parameters.html")
+                .build())
+        .map(SupportedResource::asConformanceResource)
         .collect(toList());
   }
 
@@ -59,6 +90,11 @@ public class MetadataController {
             .build());
   }
 
+  private static List<Conformance.Rest> restDstu2() {
+    return List.of(
+        Conformance.Rest.builder().mode(RestMode.server).resource(resourcesDstu2()).build());
+  }
+
   private CapabilityStatement.Implementation implementation() {
     return CapabilityStatement.Implementation.builder()
         .description(NAME)
@@ -66,7 +102,13 @@ public class MetadataController {
         .build();
   }
 
-  @GetMapping
+  private Conformance.Implementation implementationDstu2() {
+    return Conformance.Implementation.builder().description(NAME).url(pageLinks.r4Url()).build();
+  }
+
+  @GetMapping(
+      path = "r4/metadata",
+      produces = {"application/json", "application/fhir+json"})
   CapabilityStatement read() {
     return CapabilityStatement.builder()
         .resourceType("CapabilityStatement")
@@ -89,8 +131,40 @@ public class MetadataController {
         .build();
   }
 
+  @GetMapping(
+      path = "dstu2/metadata",
+      produces = {"application/json", "application/fhir+json"})
+  Conformance readDstu2() {
+    return Conformance.builder()
+        .resourceType("Conformance")
+        .id("smart-cards-conformance")
+        .version("1.0.2")
+        .name(NAME)
+        .publisher("Department of Veterans Affairs")
+        .status(Status.active)
+        .implementation(implementationDstu2())
+        .experimental(true)
+        .contact(contactDstu2())
+        .date(buildProperties.getTime().toString())
+        .description("Read and search support for credentials of immunization by patient.")
+        .kind(Kind.capability)
+        .software(softwareDstu2())
+        .fhirVersion("4.0.1")
+        .format(List.of("application/json", "application/fhir+json"))
+        .rest(restDstu2())
+        .build();
+  }
+
   private CapabilityStatement.Software software() {
     return CapabilityStatement.Software.builder()
+        .name(buildProperties.getGroup() + ":" + buildProperties.getArtifact())
+        .releaseDate(buildProperties.getTime().toString())
+        .version(buildProperties.getVersion())
+        .build();
+  }
+
+  private Conformance.Software softwareDstu2() {
+    return Conformance.Software.builder()
         .name(buildProperties.getGroup() + ":" + buildProperties.getArtifact())
         .releaseDate(buildProperties.getTime().toString())
         .version(buildProperties.getVersion())
@@ -103,6 +177,15 @@ public class MetadataController {
     String type;
 
     String profileUrl;
+
+    Conformance.RestResource asConformanceResource() {
+      return RestResource.builder()
+          .type(type)
+          .profile(Reference.builder().reference(profileUrl).build())
+          .interaction(interactionsDstu2())
+          .versioning(RestResourceVersion.no_version)
+          .build();
+    }
 
     CapabilityStatement.CapabilityResource asResource() {
       return CapabilityStatement.CapabilityResource.builder()
@@ -121,6 +204,15 @@ public class MetadataController {
       CapabilityStatement.ResourceInteraction readable =
           CapabilityStatement.ResourceInteraction.builder()
               .code(CapabilityStatement.TypeRestfulInteraction.read)
+              .documentation("Implemented per specification. See http://hl7.org/fhir/R4/http.html")
+              .build();
+      return List.of(readable);
+    }
+
+    private List<Conformance.ResourceInteraction> interactionsDstu2() {
+      Conformance.ResourceInteraction readable =
+          Conformance.ResourceInteraction.builder()
+              .code(ResourceInteractionCode.read)
               .documentation("Implemented per specification. See http://hl7.org/fhir/R4/http.html")
               .build();
       return List.of(readable);

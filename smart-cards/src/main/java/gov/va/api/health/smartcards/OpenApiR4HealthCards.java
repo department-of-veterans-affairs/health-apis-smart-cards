2f8de947a07b741cd3b7dffef9f188a406941a21
Alberto Peirats <72031660+apeirats-va@users.noreply.github.com>
2021-04-19 19:58:15 -0400

    rev fhir resources and swagger versions

--- smart-cards/src/main/java/gov/va/api/health/smartcards/OpenApiR4HealthCards.java
+++ smart-cards/src/main/java/gov/va/api/health/smartcards/OpenApiR4HealthCards.java
@@ -1,21 +1,43 @@
 package gov.va.api.health.smartcards;
 
+import gov.va.api.health.r4.api.resources.OperationOutcome;
 import gov.va.api.health.r4.api.resources.Parameters;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import org.springframework.http.ResponseEntity;
 
 public interface OpenApiR4HealthCards {
   @POST
   @Operation(summary = "Issues Verifiable Credential")
   @Path("Patient/{id}/$health-cards-issue")
-  @ApiResponse(responseCode = "200", description = "Verifiable Credential Received")
-  ResponseEntity<Parameters> healthCardsIssue(
+  @ApiResponse(
+      responseCode = "200",
+      description = "Verifiable Credential Received",
+      content =
+          @Content(
+              mediaType = "application/json",
+              schema = @Schema(implementation = Parameters.class)))
+  @ApiResponse(
+      responseCode = "400",
+      description = "Bad request",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  @ApiResponse(
+      responseCode = "404",
+      description = "Not found",
+      content =
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class)))
+  Parameters healthCardsIssue(
       @Parameter(
               in = ParameterIn.PATH,
               name = "id",
